9be24dcaafb34f95e8ca565bd44a33ae1ce370be
Curso mañana <Curso maana@PC11a5>
2018-02-09 14:25:58 +0100

    Borrar reservas

---
+++ Spring5_Security/src/main/java/com/boraji/tutorial/spring/BorrarReservas.java
@@ -0,0 +1,55 @@
+package com.boraji.tutorial.spring;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BorrarReservas
+ */
+public class BorrarReservas extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BorrarReservas() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int idreserva = Integer.parseInt(request.getParameter("idreserva"));
+
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/viajes", "root", "");
+			PreparedStatement pstmt2;
+			pstmt2 = conn.prepareStatement("delete from reservas where idreserva=?");
+			
+			pstmt2.setInt(1, idreserva);
+			pstmt2.execute();
+			
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}catch(SQLException e) {
+			
+			e.printStackTrace();
+			
+		}
+	}
+
+}
